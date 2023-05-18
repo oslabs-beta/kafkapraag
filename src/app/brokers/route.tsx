db5ba76557ffe6bdb54fed9e152634cb19eabb20
sherrii <sherrylu710@gmail.com>
2023-05-18 19:43:08 -0400

    added 2 fetch reqs from back end to jolokia

---
+++ src/app/brokers/route.tsx
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+
+   // fetch from jolokia localhost 8787
+   const response = await fetch('http://localhost:8778/jolokia/read/kafka.server:type=BrokerTopicMetrics,name=TotalProduceRequestsPerSec');
+   // parse the response
+   let produceRequestRate = await response.json();
+      // console log the response, access what to send back
+   console.log(produceRequestRate.value.OneMinuteRate)
+   // send some of the data to frontend
+   // OneMinuteRate, 
+   return NextResponse.json({ OneMinuteProduceRequestRate: produceRequestRate.value.OneMinuteRate});
+}
+ 
+export async function POST() {
+   return NextResponse.json({ message: "Goodbye from server!" });
+}
+ 
+export async function PUT() {
+   return NextResponse.json({ message: "F off from server!" });
+}
+ 
+export async function DELETE() {
+   return NextResponse.json({ message: "Later from server!" });
+}
