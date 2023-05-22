ef1a60d6eabe493419dbffd53711bc39b2737128
sherrii <sherrylu710@gmail.com>
2023-05-22 17:09:50 -0400

    pull from dev

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	esModule: true,
-};
+  reactStrictMode: true,
+  esModule: true
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
