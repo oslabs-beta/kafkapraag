7338267a4aa084d7c008babdda90419acf73e74a
Camina Drummer <drummer.camina@pm.me>
2023-05-24 18:28:33 -0400

    Fix issue with routes caching Jolokia fetches

--- src/app/api/bytes/route.tsx
+++ src/app/api/bytes/route.tsx
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET(req, res) {
    // fetch from jolokia localhost 8787
-   const response = await fetch('http://127.0.0.1:8778/jolokia/read/kafka.server:type=BrokerTopicMetrics,name=BytesInPerSec');
+   const response = await fetch('http://127.0.0.1:8778/jolokia/read/kafka.server:type=BrokerTopicMetrics,name=BytesInPerSec', { cache: 'no-store' });
    const byteIn = await response.json();
    const bytes : number = byteIn.value.OneMinuteRate;
    const count : number = byteIn.value.Count;
