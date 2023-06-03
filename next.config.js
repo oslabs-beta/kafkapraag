bccf3b37e93f1a1d81ff9cecee06c1a0861b6796
Camina Drummer <drummer.camina@pm.me>
2023-06-02 20:53:40 -0400

    Fix NextAuth issues, requires workaround in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   typescript: {
+    // Necessary for building with nextauth due to authOptions being an invalid route export type
+    // Remove once NextAuth (Auth.js moving forward) better integrates this with Next.js 13!
     ignoreBuildErrors: true
   }
 }
