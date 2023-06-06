34e768a36d9d9e1f85f1110288a4c6e67aef4231
Hank McGill <henrymcgill@gmail.com>
2023-06-06 18:55:50 -0400

    added authorization to all rendered pages

--- src/app/(postauth)/producers/page.tsx
+++ src/app/(postauth)/producers/page.tsx
@@ -1,14 +1,26 @@
+'use client'
 import ProducersRequestRateGraph from '@components/ProducersRequestsGraph'
+import { signIn, useSession } from 'next-auth/react'
 
 const ProducersRequestRate: React.FC = () => {
-  return (
+  const { data: session } = useSession()
+
+  if (session === null) {
+    return (<div className='md:mr-80 mt-80 flex flex-wrap justify-center '>    <button className='btn btn-outline btn-accent' onClick={(e) => {
+      signIn({ callbackUrl: 'http://localhost:3000/overall' })
+        .catch((err) => { console.log(err) })
+    }}>You are not currently authorized to view this page.  Click here to sign in</button>
+  </div>)
+  } else {
+    return (
     <div className="flex-column">
       <p className="text-center text-4xl font-light">Producers Request Rate</p>
       <div className="flex justify-center mt-10">
         <ProducersRequestRateGraph/>
       </div>
     </div>
-  )
+    )
+  }
 }
 
 export default ProducersRequestRate
--- src/app/(postauth)/topics/page.tsx
+++ src/app/(postauth)/topics/page.tsx
@@ -1,15 +1,26 @@
 'use client'
 import Topics from '@components/Topics'
+import { signIn, useSession } from 'next-auth/react'
 
 const Topic: React.FC = () => {
-  return (
+  const { data: session } = useSession()
+
+  if (session === null) {
+    return (<div className='md:mr-80 mt-80 flex flex-wrap justify-center '>    <button className='btn btn-outline btn-accent' onClick={(e) => {
+      signIn({ callbackUrl: 'http://localhost:3000/overall' })
+        .catch((err) => { console.log(err) })
+    }}>You are not currently authorized to view this page.  Click here to sign in</button>
+  </div>)
+  } else {
+    return (
     <div className="flex-column">
       <p className="text-center text-4xl font-light">Producers Request Rate</p>
       <div className="flex justify-center shadow-none border-none">
         <Topics/>
       </div>
     </div>
-  )
+    )
+  }
 }
 
 export default Topic
