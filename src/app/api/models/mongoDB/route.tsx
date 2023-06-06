f4118b025a3c13a69b256ff9cd271fc90f54d503
Hank McGill <henrymcgill@gmail.com>
2023-06-06 12:14:26 -0400

    added mongodb connection to nextauth options

--- src/app/api/models/mongoDB/route.tsx
+++ src/app/api/models/mongoDB/route.tsx
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import { type MongoClient } from 'mongodb'
+import { MongoClient } from 'mongodb'
 
 export async function GET (): Promise<NextResponse> {
   // const response = await fetch('')
@@ -13,23 +13,7 @@ if (!process.env.MONGODB_URI) {
 const uri = process.env.MONGODB_URI
 const options = {}
 
-let client
-let clientPromise: Promise<MongoClient>
+const client = new MongoClient(uri, options)
+const clientPromise: Promise<MongoClient> = client.connect()
 
-// if (process.env.NODE_ENV === "development") {
-//   // In development mode, use a global variable so that the value
-//   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-//   if (!global._mongoClientPromise) {
-//     client = new MongoClient(uri, options);
-//     global._mongoClientPromise = client.connect();
-//   }
-//   clientPromise = global._mongoClientPromise;
-// } else {
-//   // In production mode, it's best to not use a global variable.
-//   client = new MongoClient(uri, options);
-//   clientPromise = client.connect();
-// }
-
-// // Export a module-scoped MongoClient promise. By doing this in a
-// // separate module, the client can be shared across functions.
-// export default clientPromise;
+export default clientPromise
