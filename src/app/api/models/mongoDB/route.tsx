7e936c0fe0897622b089fb759a4ff65dce015f11
Hank McGill <henrymcgill@gmail.com>
2023-06-06 13:43:29 -0400

    added storage of user data in mongodb collection

--- src/app/api/models/mongoDB/route.tsx
+++ src/app/api/models/mongoDB/route.tsx
@@ -1,19 +1,21 @@
-import { NextResponse } from 'next/server'
+// import { NextResponse } from 'next/server'
 import { MongoClient } from 'mongodb'
 
-export async function GET (): Promise<NextResponse> {
-  // const response = await fetch('')
-  return NextResponse.json({ hello: 'hello there!' })
-}
+// export async function GET (): Promise<NextResponse> {
+//   // const response = await fetch('')
+//   return NextResponse.json({ hello: 'hello there!' })
+// }
 
 // if (!process.env.MONGODB_URI) {
 //   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
 // }
 
-const uri = process.env.MONGODB_URI || 'mongodb://test'
+const uri = process.env.MONGODB_URI
 const options = {}
 
 const client = new MongoClient(uri, options)
 const clientPromise: Promise<MongoClient> = client.connect()
+
+console.log('Pinged your deployment. You successfully connected to MongoDB!')
 
 export default clientPromise
