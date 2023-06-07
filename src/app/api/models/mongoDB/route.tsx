ab9d0501e88322b2d1f44cb9f54d87353fad9a35
Hank McGill <henrymcgill@gmail.com>
2023-06-06 20:12:01 -0400

    refactored and added types to postauth components

--- src/app/api/models/mongoDB/route.tsx
+++ src/app/api/models/mongoDB/route.tsx
@@ -1,20 +1,11 @@
-// import { NextResponse } from 'next/server'
 import { MongoClient } from 'mongodb'
 
-// export async function GET (): Promise<NextResponse> {
-//   // const response = await fetch('')
-//   return NextResponse.json({ hello: 'hello there!' })
-// }
-
-// if (!process.env.MONGODB_URI) {
-//   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
-// }
-
-const uri = process.env.MONGODB_URI
+// set up a connection uri: https://www.mongodb.com/docs/manual/reference/connection-string/
+const uri: string = process.env.MONGODB_URI as string
 const options = {}
 
 const client = new MongoClient(uri, options)
 const clientPromise: Promise<MongoClient> = client.connect()
-console.log('Pinged your deployment. You successfully connected to MongoDB!')
+console.log('Successfully connected to MongoDB')
 
 export default clientPromise
