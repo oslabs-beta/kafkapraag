822f6a6bb4cf7c659b28fdfc91fa301c61089574
Hank McGill <henrymcgill@gmail.com>
2023-06-08 09:54:34 -0400

    made mongodb auth optional

--- src/app/api/models/mongoDB/route.tsx
+++ src/app/api/models/mongoDB/route.tsx
@@ -1,10 +1,17 @@
-import { MongoClient } from 'mongodb'
 // set up a connection uri: https://www.mongodb.com/docs/manual/reference/connection-string/
-const uri: string = process.env.MONGODB_URI as string
-const options = {}
 
-const client = new MongoClient(uri, options)
-const clientPromise: Promise<MongoClient> = client.connect()
-console.log('Connected to MongoDB')
+// AFTER SETTING UP A MONGODB CONNECTION URI, COMMENT
+// THE FOLLOWING TEXT BACK IN:
 
-export default clientPromise
+// import { MongoClient } from 'mongodb'
+
+// const uri: string = process.env.MONGODB_URI as string
+// const options = {}
+
+// const client = new MongoClient(uri, options)
+// const clientPromise: Promise<MongoClient> = client.connect()
+// console.log('Connected to MongoDB')
+
+// export default clientPromise
+
+// NOW NAVIGATE TO ./auth/[...nextauth]/route.ts
